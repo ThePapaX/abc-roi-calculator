794eed8ace3124fe94aa3419b292878e049f1553
Armando Lozada <arloznav.sis@gmail.com>
2020-09-04 11:22:35 +1000

    Added unit tests project

---
+++ src/server/AbcRoiCalculator.Test/CurrencyConverterTests.cs
@@ -0,0 +1,80 @@
+﻿using AbcRoiCalculatorApp.Models;
+using ExchangeRateServiceClient;
+using ExchangeRatesService;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbcRoiCalculator.Test
+{
+    class CurrencyConverterTests
+    {
+        private const double _audUsdConversionRate = 0.75;
+        private const string _baseCurrency = "AUD";
+        private const string _targetCurrency = "USD";
+        private static IExchangeRatesProvider GetGrpcClientMock()
+        {
+            var mockFX = new Mock<IExchangeRatesProvider>();
+            mockFX.Setup(instance => instance.GetRates(It.IsAny<string>())).Returns(() =>
+            {
+                var ratesResponse = new RatesResponse()
+                {
+                    BaseCurrency = _baseCurrency,
+
+                };
+                var rates = new Dictionary<string, double>()
+                {
+                    { _targetCurrency , _audUsdConversionRate }
+                };
+                ratesResponse.Rates.Add(rates);
+
+                return Task.FromResult(ratesResponse);
+
+            });
+
+            return mockFX.Object;
+        }
+
+        [Test]
+        public void CanBeInstantiated()
+        {
+            // Arrange
+            var grpcClientMock = GetGrpcClientMock();
+
+            // Act
+            var currencyConverter = new CurrencyConverter(grpcClientMock);
+
+            // Assert
+            Assert.Pass();
+        }
+
+        [Test]
+        public async Task ConvertsCurrency()
+        {
+            // Arrange
+            var grpcClientMock = GetGrpcClientMock();
+
+            // Act
+            var currencyConverter = new CurrencyConverter(grpcClientMock);
+            
+            const double expectedTotalRoi = 750;
+            const double expectedTotalFees = 75;
+
+            var roiResult = new RoiCalculationResult() {
+                Currency = _baseCurrency,
+                Total = 1000,
+                Fees = 100,
+            };
+
+
+            await currencyConverter.Convert(roiResult, _baseCurrency, _targetCurrency);
+
+            // Assert
+            Assert.AreEqual(roiResult.Total, expectedTotalRoi);
+            Assert.AreEqual(roiResult.Fees, expectedTotalFees);
+        }
+    }
+}
---
+++ src/server/AbcRoiCalculator.Test/InvestmentOptionRuleTests.cs
@@ -0,0 +1,74 @@
+﻿using AbcRoiCalculatorApp.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AbcRoiCalculator.Test
+{
+    [TestFixture]
+    class InvestmentOptionRuleTests
+    {
+        [TestCase(0.1, 0.45)]
+        [TestCase(0, 0)]
+        [TestCase(1, 1)]
+        public void LowerBoundTest(double lowerBound, double upperBound)
+        {
+            // Arrange
+            var optionRule = new InvestmentOptionRule()
+            {
+                From = lowerBound,
+                To = upperBound
+            };
+
+            // Act
+            var isApplicable = optionRule.IsApplicableForProportion(lowerBound);
+
+            // Assert
+            Assert.IsTrue(isApplicable);
+            
+        }
+
+        [TestCase(0, 0.66666)]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        public void UpperBoundTest(double lowerBound, double upperBound)
+        {
+            // Arrange
+            var optionRule = new InvestmentOptionRule()
+            {
+                From = lowerBound,
+                To = upperBound
+            };
+
+            // Act
+            var isApplicable = optionRule.IsApplicableForProportion(upperBound);
+
+            // Assert
+            Assert.IsTrue(isApplicable);
+
+        }
+
+        [TestCase(0, 0, .5)]
+        [TestCase(0, 1, -1)]
+        [TestCase(0, 1, 1.0001)]
+        [TestCase(.1, .3, .31)]
+        public void OutOfBoundNotApplicatbleTest(double lowerBound, double upperBound, double proportion)
+        {
+            // Arrange
+            var optionRule = new InvestmentOptionRule()
+            {
+                From = lowerBound,
+                To = upperBound
+            };
+
+            // Act
+            var isApplicable = optionRule.IsApplicableForProportion(proportion);
+
+            // Assert
+            Assert.IsFalse(isApplicable);
+
+        }
+
+    }
+}
