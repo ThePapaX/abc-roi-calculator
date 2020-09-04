add6df6733ac188a9590632f254ee9eb22d845ad
Armando Lozada <arloznav.sis@gmail.com>
2020-09-04 11:23:27 +1000

    Abstracted the Currecy conversition to a separate service. And testability improvements

---
+++ src/server/AbcRoiCalculatorApp/Models/CurrencyConverter.cs
@@ -0,0 +1,32 @@
+﻿using ExchangeRateServiceClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbcRoiCalculatorApp.Models
+{
+    public class CurrencyConverter : ICurrencyConverter
+    {
+        readonly IExchangeRatesProvider _exchangeRateClient;
+        public CurrencyConverter(IExchangeRatesProvider exchangeRateServiceClient)
+        {
+            _exchangeRateClient = exchangeRateServiceClient;
+        }
+        private async Task<double> GetConversionRate(string baseCurrency, string targetCurrency)
+        {
+            var ratesResponse = await _exchangeRateClient.GetRates(baseCurrency);
+            ratesResponse.Rates.TryGetValue(targetCurrency, out double conversionRate);
+
+            return conversionRate;
+        }
+
+        public async Task Convert(RoiCalculationResult roi, string baseCurrency, string targetCurrency)
+        {
+            var conversionRate = await GetConversionRate(baseCurrency, targetCurrency);
+            roi.Currency = targetCurrency;
+            roi.Total *= conversionRate;
+            roi.Fees *= conversionRate;
+        }
+    }
+}
---
+++ src/server/AbcRoiCalculatorApp/Models/ICurrencyConverter.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace AbcRoiCalculatorApp.Models
+{
+    public interface ICurrencyConverter
+    {
+        Task Convert(RoiCalculationResult roi, string baseCurrency, string targetCurrency);
+    }
+}
