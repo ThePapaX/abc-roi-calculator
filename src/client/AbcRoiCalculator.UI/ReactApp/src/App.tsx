dd72248c10f18055e5218c589ba02e28a5277b8a
Armando Lozada <arloznav.sis@gmail.com>
2020-09-08 09:58:23 +1000

    Removed unused dependencies.

--- src/client/AbcRoiCalculator.UI/ReactApp/src/App.tsx
+++ src/client/AbcRoiCalculator.UI/ReactApp/src/App.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import Layout from './components/Layout';
+import { Route } from 'react-router';
 import RoiCalculator  from './components/RoiCalculator'
 import './custom.css'
 
 export default () => (
     <Layout>
-        <RoiCalculator/>
+        <Route exact path='/' component={RoiCalculator} />
     </Layout>
 );
